e321cad04e1e89b4fb2a76c6a3109d2ae04e8a7b
Greg Ferrell <gregerg@gmail.com>
2014-11-17 20:59:22 -0800

    removed extra helper functions

--- CLI/helpers.js
+++ CLI/helpers.js
@@ -4,40 +4,6 @@
 var fs = require('fs');
 var yaml = require('../node_modules/js-yaml');
 
-
-/**
-* Function that add spaces to a string - using it to format the Dockerfile
-* @function
-* @memberof module:helpers
-* @param {string} str String that needs spaces appended to it
-* @param {number} num Number of spaces to add
-*/
-exports.addSpace = function(str,num) {
-  // console.log(num);
-  for(var i=0;i<num;i++) {
-    str += ' ';
-  }
-  return str;
-};
-
-/**
-* Function that returns true/false based on existence of filename (and filepath)
-* @function
-* @memberof module:helpers
-* @param {string} filename Path and filename that is checked by function
-*/
-exports.doesFileExist = function(filename) {
-  return fs.open(filename,'r',function(err,fd) {
-    if(err) {
-      console.log('false');
-      return false;
-    } else {
-      console.log('true');
-      return true;
-    }
-  });
-};
-
 /**
 * Function that reads the lifter.yml file
 * @function
@@ -47,6 +13,5 @@ exports.doesFileExist = function(filename) {
 
 exports.readYAML = function() {
   var content = fs.readFileSync('lifter.yml', {encoding: 'utf-8'});
-  var ymlContents = yaml.safeLoad(content);
-  return ymlContents;
+  return yaml.safeLoad(content);
 }
