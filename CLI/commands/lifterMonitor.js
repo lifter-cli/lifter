6ac78e9956e41ecb9078e95e75b02480115a8750
willchen90 <willchen90@gmail.com>
2014-11-21 20:33:24 -0800

    added lifter monitor command

---
+++ CLI/commands/lifterMonitor.js
@@ -0,0 +1,19 @@
+var exec = require('child_process').exec;
+
+// copies the mounted volume (from /src) into a new directory(/prod)
+var lifterMonitor = function() {
+  // src is where the mounted files exist
+  // app is where the copied files will be transeffered to
+  var command = './lifter-ui/run-lifter-ui.sh';
+  exec(command, function(err, stdout, stderr){
+    if(err){
+      console.log("ERR: ", stderr);
+    } else {
+      console.log("Launched lifter container. Go to http://localhost:3123");
+    }
+  });
+};
+
+module.exports = {
+  lifterMonitor: lifterMonitor
+};
