c4711bc2059c0cf39dd58ea0e585680b65be6af5
Greg Ferrell <gregerg@gmail.com>
2014-11-21 17:09:17 -0800

    remove spawnSeries from helpers.js

--- CLI/helpers/helpers.js
+++ CLI/helpers/helpers.js
@@ -3,7 +3,46 @@
 */
 var fs = require('fs');
 var yaml = require('../../node_modules/js-yaml');
-var configFile = '../../.lifter/lifter.yml';
+var configFile = '../../.lifter/lifter.yml'
+
+/**
+* Function that takes in an array of tasks that will be individually
+*   passed to the spawn function, then runs each task in series. At the 
+*   end of all the tasks, a final callback will be invoked.
+* @function
+* @memberof module:helpers
+* EXAMPLE
+*   spawnSeries([
+*    ['ls', ['-lh', '/usr']],
+*    ['ls', ['-la']]
+*   ], function() { console.log('all done!'); });
+*/
+var spawnSeries = function(tasks, callback) {
+  callback = callback || function() {};
+  var completed = 0;
+  var iterate = function() {
+    if (++completed < tasks.length) {
+      run();
+    } else {
+      callback();
+    }
+  }
+  var run = function() {
+    var task = tasks[completed];
+    var proc = spawn.apply(null, task);
+    proc.stdout.on('data', function(data) {
+      console.log(data.toString());
+    });
+    proc.stderr.on('data', function(data) {
+      console.log(data.toString());
+    });
+    proc.on('exit', function(code) {
+      // console.log('EXIT CODE:', code);
+      iterate();
+    });
+  }
+  run();
+}
 
 /**
 * Function that reads the lifter.yml file
@@ -27,6 +66,7 @@ var readYAML = function() {
 }
 
 module.exports = {
+  spawnSeries: spawnSeries,
   readYAML: readYAML,
   configFile: configFile
 }
