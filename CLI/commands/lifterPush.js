5a8d32156404e3e72ff487365e1e1bcc6b601c05
Greg Ferrell <gregerg@gmail.com>
2014-11-25 22:29:35 -0800

    refactor lifter push to use docker series library command

--- CLI/commands/lifterPush.js
+++ CLI/commands/lifterPush.js
@@ -1,56 +1,42 @@
-var fs = require('fs');
-var exec = require('child_process').exec;
 var yaml = require('../../node_modules/js-yaml');
 var helper = require('../helpers/helpers.js');
+var docker = require('../helpers/dockerLib.js');
 
-// copies the mounted volume (from /src) into a new directory(/prod)
-var copyMounted = function() {
-  console.log('Copying source code into container');
 
+var runLifterPush = function() {
   var yamlContent = helper.readYAML();
+  
+  // copies the mounted volume (from /src) into a new directory(/prod)
   // src is where the mounted files exist
   // app is where the copied files will be transeffered to
-  var command = 'docker exec ' + yamlContent.containerName +' cp -r src/ /prod';
-  exec(command, function(err, stdout, stderr){
-    if(err){
-      console.log("ERR: ", stderr);
-    } else {
-      console.log("Source code copied: /prod");
-      console.log('Commiting changes to container...');
-      commitImage();
-    }
-  });
-};
+  var copy = ['docker', [
+    'exec', yamlContent.containerName, 
+    'cp', '-r', 'src/', '/prod'
+  ]];
 
-// commits changes and creates a docker image
-var commitImage = function() {
-  var yamlContent = helper.readYAML();
-  var command = 'docker commit ' + yamlContent.containerName + ' ' + yamlContent.username + '/' + yamlContent.repoName + ':latest';
-  exec(command, function(err, stdout, stderr){
-    if(err){
-      console.log("ERR: ", stderr);
-    } else {
-      console.log("Pushing changes to Docker Hub...");
-      pushImage();
-    }
-  });
-};
+  // commits changes and creates a docker image
+  var commit = ['docker', [
+    'commit', yamlContent.containerName, 
+    yamlContent.username+'/'+yamlContent.repoName+':latest'
+  ]];
 
-// pushes docker image to docker hub
-var pushImage = function() {
-  var yamlContent = helper.readYAML();
-  var command = 'docker push ' + yamlContent.username + '/' + yamlContent.repoName + ':latest';
-  exec(command, function(err, stdout, stderr){
-    if(err){
-      console.log("ERR: ", stderr);
-    } else {
-      console.log("Your container's latest state is now on Docker Hub.\nType 'lifter deploy' to deploy your application containers.");
-    }
+  // pushes docker image to docker hub
+  var push = ['docker', [
+    'push', yamlContent.username+'/'+yamlContent.repoName+':latest'
+  ]];
+
+  docker.spawnSeries([
+    copy,
+    commit,
+    push
+  ], function() {
+    console.log("Your container's latest state is now on Docker Hub.\nType 'lifter deploy' to deploy your application containers.");
   });
-};
+}
+
 
 module.exports = {
-  copyMounted: copyMounted
+  copyMounted: runLifterPush
 }
 
 
