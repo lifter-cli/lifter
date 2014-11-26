34c1d2c55dfcdc1bca01e9e6a7fe50b32d360608
willchen90 <willchen90@gmail.com>
2014-11-26 11:41:39 -0800

    more flexible dockerode API settings

--- lifter-ui/server/api/dockerRemoteAPI.js
+++ lifter-ui/server/api/dockerRemoteAPI.js
@@ -5,18 +5,27 @@ var Docker = require('dockerode');
 // // var docker4 = new Docker({host: '127.0.0.1', port: 3000}); //defaults to http
 // //
 
-var caPath = process.env.DOCKER_CA_PATH || (process.env.DOCKER_CERT_PATH + '/ca.pem');
-var certPath = process.env.DOCKER_CERT_PATH || (process.process.env.DOCKER_CERT_PATH + '/cert.pem');
-var keyPath = process.env.DOCKER_KEY_PATH || (process.env.DOCKER_CERT_PATH + '/key.pem');
+var caPath = process.env.DOCKER_MONITOR_CA_PATH || (process.env.DOCKER_CERT_PATH + '/ca.pem');
+var certPath = process.env.DOCKER_MONITOR_CERT_PATH || (process.env.DOCKER_CERT_PATH + '/cert.pem');
+var keyPath = process.env.DOCKER_MONITOR_KEY_PATH || (process.env.DOCKER_CERT_PATH + '/key.pem');
+var host = process.env.DOCKER_MONITOR_HOST_IP || '192.168.59.103';
 
 var fs = require('fs');
-var docker = new Docker({
-  protocol: 'https',
-  host: '192.168.59.103',
-  port: process.env.DOCKER_PORT || 2376,
-  ca: fs.readFileSync(caPath),
-  cert: fs.readFileSync(certPath),
-  key: fs.readFileSync(keyPath)
-});
+var dockerSettings = {};
+if ( process.env.AZURE_UBUNTU ) {
+  dockerSettings = {
+    socketPath: '/var/run/docker.sock'
+  };
+} else {
+  dockerSettings = {
+    protocol: 'https',
+    host: host,
+    port: process.env.DOCKER_PORT || 2376,
+    ca: fs.readFileSync(caPath),
+    cert: fs.readFileSync(certPath),
+    key: fs.readFileSync(keyPath)
+  };
+}
+var docker = new Docker(dockerSettings);
 
 exports.docker = docker;
